fc9bff8bf10f2c81ff8c4dbdb5d87266b51c4c4a
Filipe <filipeseventeen1@gmail.com>
2021-12-19 10:39:35 -0300

    Feito a tela inicial

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { SafeAreaView, StatusBar, View, Text, TouchableOpacity } from 'react-native'
+
+import { FontAwesome } from '@expo/vector-icons'
+
+import { styles } from './app-style'
+import { cssvar_primary } from './css_variables'
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor={cssvar_primary} barStyle="dark-content" />
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Controle suas finanças de um jeito rápido e prático
+        </Text>
+        <TouchableOpacity style={styles.iconButton}>
+          <FontAwesome style={styles.icon} name="arrow-right" size={44} />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
---
+++ css_variables.ts
@@ -0,0 +1,3 @@
+export const cssvar_dark = '#2A2A2A'
+export const cssvar_text = '#E6EAF9'
+export const cssvar_primary = '#E6DCF5'
